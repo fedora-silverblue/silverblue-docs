e73b9a49195b596098346f1a80db5a858fc500a6
Stefano Figura <stefano@figura.im>
2019-06-05 18:43:12 +0200

    Add `podman` for build and preview
    Fix repo sync issue

--- preview.sh
+++ preview.sh
@@ -1,18 +1,47 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]; then
+cmd="run --rm -v $(pwd):/antora:ro -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf:ro -p 8080:80 nginx"
+
+container_launch() {
+    # Use podman if available on Linux, otherwise use docker
+    if hash podman 2>/dev/null; then
+        echo "Podman is launching the preview."
+        echo "The preview will be available at http://localhost:8080/"
+        podman $1
+
+    elif hash docker 2>/dev/null; then
+    
+	if groups | grep -wq "docker"; then
+            echo "Docker is launching the preview in an isolated environment."
+	    echo "The preview will be available at http://localhost:8080/"
+            docker $1
+
+        else
+            echo "This preview script is using $runtime to run the preview in an isolated environment. You might be asked for your password."
+            echo "You can avoid this by adding your user to the 'docker' group, but be aware of the security implications. See https://docs.docker.com/install/linux/linux-postinstall/."
+	    echo "The preview will be available at http://localhost:8080/"
+            sudo docker $1
+        fi
+
+    else
+        echo "Error: No container runtimes were found."
+        echo "Please install either podman or docker"
+        exit 1
+    fi
+}
+
+if [ "$(uname)" = "Darwin" ]; then
     # Running on macOS.
     # Let's assume that the user has the Docker CE installed
     # which doesn't require a root password.
-    echo "The preview will be available at http://localhost:8080/"
-    docker run --rm -v $(pwd)/public:/usr/share/nginx/html:ro -p 8080:80 nginx
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Running on Linux.
-    # Let's assume that it's running the Docker deamon
-    # which requires root.
     echo ""
-    echo "This build script is using Docker to run the build in an isolated environment. You might be asked for a root password in order to start it."
     echo "The preview will be available at http://localhost:8080/"
-    sudo docker run --rm -v $(pwd)/public:/usr/share/nginx/html:ro -p 8080:80 nginx
+    echo ""
+    docker "$cmd"
+
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    # Running on Linux.
+    # Run the preview using the container_launch function
+    echo ""
+    container_launch "$cmd"
 fi
