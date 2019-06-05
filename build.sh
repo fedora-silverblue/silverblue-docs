be63177416161f7d8a73337d7fd88b76989ddd4d
Stefano Figura <stefano@figura.im>
2019-06-05 18:43:12 +0200

    Add `podman` for build and preview
    Fix repo sync issue

--- build.sh
+++ build.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]; then
+cmd="run --rm -it -v $(pwd):/antora:z docker.io/antora/antora --html-url-extension-style=indexify site.yml"
+
+container_launch() {
+    # Use podman if available on Linux, otherwise use docker
+    if hash podman 2>/dev/null; then
+        echo "This build script is using Podman to run the build in an isolated environment."
+        podman $1
+
+    elif hash docker 2>/dev/null; then
+
+        if groups | grep -wq "docker"; then
+            echo "This build script is using Docker to run the build in an isolated environment."
+            docker $1
+        
+        else
+            echo "This build script is using $runtime to run the build in an isolated environment. You might be asked for your password."
+            echo "You can avoid this by adding your user to the 'docker' group, but be aware of the security implications. See https://docs.docker.com/install/linux/linux-postinstall/."
+            sudo docker $1
+        fi
+
+    else
+   	echo "Error: No container runtimes were found."
+	echo "Please install either podman or docker"
+	exit 1
+    fi
+}
+
+if [ "$(uname)" = "Darwin" ]; then
     # Running on macOS.
     # Let's assume that the user has the Docker CE installed
     # which doesn't require a root password.
-    docker run --rm -it -v $(pwd):/antora antora/antora --html-url-extension-style=indexify site.yml
+    echo "This build script is using Docker container runtime to run the build in an isolated environment."
+    docker run $cmd
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     # Running on Linux.
-    # Let's assume that it's running the Docker deamon
-    # which requires root.
-    echo ""
-    echo "This build script is using Docker to run the build in an isolated environment. You might be asked for a root password in order to start it."
-sudo docker run --rm -it -v $(pwd):/antora:z antora/antora --html-url-extension-style=indexify site.yml
+    # Build the documentation using the container_launch function
+    container_launch "$cmd"
 fi
