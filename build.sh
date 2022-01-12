de85095427f0575af8ac8ff665c0d96e3cff50d0
Timothée Ravier <tim@siosm.fr>
2022-01-12 16:11:32 +0100

    build & preview: Update with latest version from template

--- build.sh
+++ build.sh
@@ -1,41 +1,51 @@
 #!/bin/sh
 
-cmd="run --rm -it -v $(pwd):/antora:z docker.io/antora/antora --html-url-extension-style=indexify site.yml"
+image="docker.io/antora/antora"
+cmd="--html-url-extension-style=indexify site.yml"
 
-container_launch() {
-    # Use podman if available on Linux, otherwise use docker
-    if hash podman 2>/dev/null; then
-        echo "This build script is using Podman to run the build in an isolated environment."
-        podman $1
-
-    elif hash docker 2>/dev/null; then
-
-        if groups | grep -wq "docker"; then
-            echo "This build script is using Docker to run the build in an isolated environment."
-            docker $1
-
-        else
-            echo "This build script is using $runtime to run the build in an isolated environment. You might be asked for your password."
-            echo "You can avoid this by adding your user to the 'docker' group, but be aware of the security implications. See https://docs.docker.com/install/linux/linux-postinstall/."
-            sudo docker $1
-        fi
-
-    else
-        echo "Error: No container runtimes were found."
-        echo "Please install either podman or docker"
-        exit 1
-    fi
-}
-
-if [ "$(uname)" = "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
     # Running on macOS.
     # Let's assume that the user has the Docker CE installed
     # which doesn't require a root password.
+    echo ""
     echo "This build script is using Docker container runtime to run the build in an isolated environment."
-    docker run $cmd
+    echo ""
+    docker run --rm -it -v $(pwd):/antora $image $cmd
 
-elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Running on Linux.
-    # Build the documentation using the container_launch function
-    container_launch "$cmd"
+    # there isn't an antora/aarch64 container, antora can be installed locally
+    # Check whether podman is available, else faill back to docker
+    # which requires root.
+
+    if [ -f /usr/local/bin/antora ]; then
+        /usr/local/bin/antora $cmd
+    elif [[ `uname -m` == "aarch64" ]]; then
+        echo "no antora/aarch64 container try just \`npm install -g @antora/cli @antora/site-generator-default\`"
+    elif [ -f /usr/bin/podman ]; then
+        echo ""
+        echo "This build script is using Podman to run the build in an isolated environment."
+        echo ""
+        podman run --rm -it -v $(pwd):/antora:z $image $cmd
+
+    elif [ -f /usr/bin/docker ]; then
+        echo ""
+        echo "This build script is using Docker to run the build in an isolated environment."
+        echo ""
+
+        if groups | grep -wq "docker"; then
+            docker run --rm -it -v $(pwd):/antora:z $image $cmd
+        else
+            echo ""
+            echo "This build script is using $runtime to run the build in an isolated environment. You might be asked for your password."
+            echo "You can avoid this by adding your user to the 'docker' group, but be aware of the security implications. See https://docs.docker.com/install/linux/linux-postinstall/."
+            echo ""
+            sudo docker run --rm -it -v $(pwd):/antora:z $image $cmd
+        fi
+    else
+        echo ""
+        echo "Error: Container runtime haven't been found on your system. Fix it by:"
+        echo "$ sudo dnf install podman"
+        exit 1
+    fi
 fi
